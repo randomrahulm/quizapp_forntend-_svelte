f75a9f35eaee9521c8e1e15a87feb71dce1d5051
Rahul Mohan <120002742+randomrahulm@users.noreply.github.com>
2023-07-24 20:05:29 +0530

    card ui and quiz data accomplishesd

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang='ts'>
+    import quizz from "../data.json"
+</script>
+
+
+
+<div class="quiz_container w-[700px] h-screen m-auto flex justify-center items-center space-x-4"
+>
+    {#each quizz as quiz}
+        <div class="quiz_card w-[250px] shadow overflow-hidden cursor-pointer ">
+            <img class="h-44" src={quiz.imageUrl} alt="" >
+            <div class="flex item-center justify-between py-3 px-2">
+                <h1 class="font-bold">{quiz.name}</h1>
+                <p>{``}</p>
+            </div>
+        </div>
+    {/each}
+    
+
+</div>
